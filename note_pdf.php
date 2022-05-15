776b9f166e8b03d54980ccc8e0cb4f19bfb2e2bd
hatimzh <96501113+hatimzh@users.noreply.github.com>
2022-05-15 21:21:17 +0000

    Update note_pdf.php

--- note_pdf.php
+++ note_pdf.php
@@ -9,28 +9,28 @@ require('./fpdf/fpdf.php');
 $pdf = new FPDF();
 $Mention = 'NULL';
 
-    if(isset($_SESSION["nom"][$_SESSION['var']])){
+    if(isset($_SESSION["name"][$_SESSION['var']])){
 
-        $c1=$_SESSION["nom"][$_SESSION['var']];
+        $c1=$_SESSION["name"][$_SESSION['var']];
         $c2=$_SESSION["maths"][$_SESSION['var']];
-        $c3=$_SESSION["informatique"][$_SESSION['var']];
+        $c3=$_SESSION["physics"][$_SESSION['var']];
         $c4=($c3+$c2)/2;
                 if($c4<10){
-                    $Mention = 'Non validee';
+                    $Mention = 'Not validated';
                 }
                  elseif($c4>=10 && $c4<12){
-                    $Mention = 'Passable';
+                    $Mention = 'Standard pass';
                 }
                  elseif($c4>=12 && $c4<14){
-                    $Mention = 'Assez bien';
+                    $Mention = 'Honours';
                 }
                  elseif($c4>=14 && $c4<16){
-                    $Mention = 'Bien';
+                    $Mention = 'High honours';
                 }
                  elseif($c4>=16){
-                    $Mention = 'Tres bien';
+                    $Mention = 'Highest honour';
                  }
-                 else{ $Mention = 'Données manquantes'; }	
+                 else{ $Mention = 'Data missed'; }	
 
         $pdf->SetFillColor(220,220,220);
         $pdf->AddPage();
@@ -42,7 +42,7 @@ $Mention = 'NULL';
         $pdf->Cell(95,15,$c1,1,1,'C');
         $pdf->Cell(95,15,'Maths',1,0,'C');
         $pdf->Cell(95,15,$c2,1,1,'C');
-        $pdf->Cell(95,15,'Informatique',1,0,'C');
+        $pdf->Cell(95,15,'Physics',1,0,'C');
         $pdf->Cell(95,15,$c3,1,1,'C');
         $pdf->Cell(95,15,'Moyenne',1,0,'C');
         $pdf->Cell(95,15,$c4,1,1,'C');
